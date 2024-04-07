b668e003e101db9fab9aa88dee7427a826f03469
Relepega <rlpggmd@gmail.com>
2024-04-07 19:49:24 +0200

    refactor: Reverted sse route to the original thing

--- internal/webserver/events_stream.go
+++ internal/webserver/events_stream.go
@@ -5,22 +5,18 @@ import (
 	"net/http"
 )
 
-var connectedClients = make(map[string]http.ResponseWriter)
-
 func (ws *webserver) handleEventStream(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("New Client connected")
-
 	WriteDefaultHeaders(w)
 
 	for {
 		select {
 		case msg := <-ws.msgChan:
 			s := msg.String()
+
 			fmt.Fprintf(w, s)
 			w.(http.Flusher).Flush()
 
 		case <-r.Context().Done():
-			fmt.Println("Client disconnected")
 			return
 
 		default:
