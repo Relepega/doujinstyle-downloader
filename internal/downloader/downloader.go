044f3e6e3632d42d65e2362315ad9b1211f9b5c9
Relepega <rlpggmd@gmail.com>
2024-04-09 20:29:41 +0200

    feat: Added host download logic

--- internal/downloader/downloader.go
+++ internal/downloader/downloader.go
@@ -2,14 +2,17 @@ package downloader
 
 import (
 	"fmt"
+	"path/filepath"
 
 	"github.com/playwright-community/playwright-go"
+	"github.com/relepega/doujinstyle-downloader-reloaded/internal/appUtils"
 	"github.com/relepega/doujinstyle-downloader-reloaded/internal/configManager"
+	"github.com/relepega/doujinstyle-downloader-reloaded/internal/downloader/hosts"
 	"github.com/relepega/doujinstyle-downloader-reloaded/internal/downloader/services"
 	"github.com/relepega/doujinstyle-downloader-reloaded/internal/playwrightWrapper"
 )
 
-func Download(serviceName, albumID string, pwc *playwrightWrapper.PwContainer) error {
+func Download(serviceName, albumID string, progress *int8, pwc *playwrightWrapper.PwContainer) error {
 	runBeforeUnloadOpt := true
 	pageCloseOpts := playwright.PageCloseOptions{
 		RunBeforeUnload: &runBeforeUnloadOpt,
@@ -56,9 +59,27 @@ func Download(serviceName, albumID string, pwc *playwrightWrapper.PwContainer) e
 	if err != nil {
 		return err
 	}
-	defer downloadPage.Close()
+	defer downloadPage.Close(pageCloseOpts)
 
 	_ = servicePage.Close(pageCloseOpts)
+
+	dl_path := filepath.Join(cfg.Download.Directory, mediaName)
+
+	err = appUtils.CreateFolder(dl_path)
+	if err != nil {
+		return err
+	}
+
+	hostFactory, err := hosts.NewHost(downloadPage.URL())
+	if err != nil {
+		return err
+	}
+
+	host := hostFactory(downloadPage, mediaName, dl_path, progress)
+	err = host.Download()
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
